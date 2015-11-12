137b76663f8a9c7d9c6518afd627748c6785a3f0
Brian Maggi <brianmaggi@gmail.com>
2015-11-12 15:52:05 -0800

    footer again

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 			<div class="col-md-12">
 				<div class="pull-right social">
 					<a href="https://www.facebook.com/colahq"><i class="fa fa-facebook-official fa-2x"></i></a>
-					<a href="https://twitter.com/colahq"><i class="fa fa-twitter-square fa-2x"></i></a>
+					<a href="https://twitter.com/cola_inc"><i class="fa fa-twitter-square fa-2x"></i></a>
 					<a href="https://www.linkedin.com/company/cola-io"><i class="fa fa-linkedin-square fa-2x"></i></a>
 				</div>
 				<p>Copyright &copy; 2015 Cola, Inc. All rights reserved.</p>				
