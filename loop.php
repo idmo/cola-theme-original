b8d4e8edb1500445869d35e118c450fde54f82f2
Brian Maggi <brianmaggi@gmail.com>
2015-11-16 15:32:10 -0800

    Added blog pagination.

--- loop.php
+++ loop.php
@@ -26,6 +26,24 @@
 			</article>
 			<hr />
 		<?php endwhile; ?>
+		<?php echo paginate_links( $args ); ?>
+		<?php $args = array(
+					'base'               => '%_%',
+					'format'             => '?page=%#%',
+					'total'              => 1,
+					'current'            => 0,
+					'show_all'           => False,
+					'end_size'           => 1,
+					'mid_size'           => 2,
+					'prev_next'          => True,
+					'prev_text'          => __('Previous'),
+					'next_text'          => __('Next'),
+					'type'               => 'plain',
+					'add_args'           => False,
+					'add_fragment'       => '',
+					'before_page_number' => '',
+					'after_page_number'  => ''
+				); ?>		
 	<?php else: ?>
 		<p>Nothing matches your query.</p>
 	<?php  endif; ?>
